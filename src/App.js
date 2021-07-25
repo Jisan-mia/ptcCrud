5756962255f5d311e5e3ce5f7f652a2f3217bf5f
Jisan-mia <jisanmia47@gmail.com>
2021-07-26 01:37:27 +0600

    header component done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Header from "./Components/Header";
+import "./Scss/main.scss";
+
 function App() {
 	return (
-		<div className="">
-			<h1>React crud</h1>
+		<div className="container">
+			<div className="main-section">
+				<Header />
+				<div className="todo-area">Todo area</div>
+				<form className="add-todo-area">add tod do area</form>
+			</div>
 		</div>
 	);
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Header = () => {
+	const [date, setDate] = useState(new Date().toDateString());
+	console.log(date);
+	return (
+		<div className="header ">
+			<h3>{date}</h3>
+			<p>Task count</p>
+		</div>
+	);
+};
+
+export default Header;
